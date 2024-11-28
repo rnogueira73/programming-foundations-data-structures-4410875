0cfaf4bbd52d5d64299fd468480b807737ebadc8
rnogueira73 <ruben_nogueira@hotmail.com>
2024-11-28 20:17:01 +0000

    Primero 4 ejercicios

--- 02/02_03/02_03b/main.py
+++ 02/02_03/02_03b/main.py
@@ -5,7 +5,20 @@ has in a given class.
 
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
+ITEM_AT_INDEX_THREE = student_pet_count_list[3]
+print(ITEM_AT_INDEX_THREE)
+
+ITEM_THREE_FROM_BACK = student_pet_count_list[-3]
+print(ITEM_THREE_FROM_BACK)
+
 NUM_OF_STUDENTS = len(student_pet_count_list)
 print(NUM_OF_STUDENTS)
 
+SUM = 0
+for INDIVIDUAL_PET_COUNT in student_pet_count_list:
+  SUM += INDIVIDUAL_PET_COUNT
+print(SUM)
+
 # average = sum / number of items
+AVERAGE = SUM / NUM_OF_STUDENTS
+print(AVERAGE)
